05f2b7ec4400156b8bb63164694b6b92d50f667a
AKKKK~ <258607438@qq.com>
2018-01-16 17:48:22 +0800

    修改
    修改部分

--- master/test/app.py
+++ master/test/app.py
@@ -16,7 +16,7 @@ app.conf_dbsource = dict(
     host='127.0.0.1',
     port=3306,
     user='root',
-    password='zhangjun',
+    password='root',
     db='akeec'
 )
 app.conf_others = dict(
--- master/test/model.py
+++ master/test/model.py
@@ -6,7 +6,7 @@ def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
-    __table__ = 'users2'
+    __table__ = 'users'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     email = StringField(ddl='varchar(50)')
